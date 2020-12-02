da79a41528ec7577e981607626c44aac987eaca2
Davvos11 <vosdavid2@gmail.com>
2020-12-02 13:48:09 +0100

    day 2 part 2

--- day2.py
+++ day2.py
@@ -16,9 +16,17 @@ def read_input(filename: str) -> [entryType]:
     return [read_line(line) for line in lines]
 
 
-def check_password(entry: entryType):
+def check_password1(entry: entryType) -> bool:
     # entry[3] should contain at least entry[0] and at most entry[1] of entry[2]
     return entry[3].count(entry[2]) in range(entry[0], entry[1]+1)
+
+
+def check_password2(entry: entryType) -> bool:
+    # entry[3] should have entry[2] at either index entry[0]-1 or entry[1]-1 (not both)
+    char1 = entry[3][entry[0] - 1] == entry[2]
+    char2 = entry[3][entry[1] - 1] == entry[2]
+    # xor
+    return char1 ^ char2
 
 
 if __name__ == '__main__':
@@ -26,6 +34,12 @@ if __name__ == '__main__':
 
     valid = 0
     for e in input2:
-        if check_password(e):
+        if check_password1(e):
             valid += 1
     print(f"Puzzle 1: {valid} valid passwords")
+
+    valid = 0
+    for e in input2:
+        if check_password2(e):
+            valid += 1
+    print(f"Puzzle 2: {valid} valid passwords")
