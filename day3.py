758b684401a4f14de089a920c223e71686bf4091
Davvos11 <vosdavid2@gmail.com>
2020-12-03 21:53:29 +0100

    implemented day 3 part 2

--- day3.py
+++ day3.py
@@ -1,4 +1,3 @@
-
 def read_line(line: str) -> [bool]:
     return [char == '#' for char in line.strip()]
 
@@ -16,17 +15,28 @@ def contains_tree(trees: [[bool]], x: int, y: int) -> bool:
     return row[x % len(row)]
 
 
+def count_trees(trees: [[bool]], right, down):
+    x = y = tree_count = 0
+    # Loop until we reached the bottom
+    while y < len(trees):
+        # Check if there is a tree in this position
+        if contains_tree(trees, x, y):
+            tree_count += 1
+        # Go to the next position
+        x += right
+        y += down
+    return tree_count
+
+
 if __name__ == '__main__':
     input3 = read_input('input/day3')
 
-    x = y = tree_count = 0
-    # Loop until we reached the bottom
-    while y < len(input3):
-        # Check if there is a tree in this position
-        if contains_tree(input3, x, y):
-            tree_count += 1
-        # Go to the next position
-        x += 3
-        y += 1
+    print(f"Puzzle 1: {count_trees(input3, 3, 1)} trees")
 
-    print(f"Puzzle 1: {tree_count} trees")
+    print("\nPuzzle 2:")
+    puzzle2 = 1
+    for r, d in [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]:
+        count = count_trees(input3, r, d)
+        puzzle2 *= count
+        print(f"   right {r}, down {d}: {count} trees")
+    print(f"Result: {puzzle2}")
