e7987efa2d412eef8174af6b89bad1c56cb7b255
Davvos11 <vosdavid2@gmail.com>
2020-12-10 17:55:41 +0100

    implemented day 10 part 2 naively, does not work on puzzle input

--- day10.py
+++ day10.py
@@ -25,6 +25,33 @@ def get_adapter_chain(adapters: Set[int]) -> Dict[int, int]:
     return differences
 
 
+def get_adapter_chain_arrangements(adapters: Set[int]) -> [[int]]:
+    result: [[int]] = []
+    target = max(adapters) + 3
+
+    # Initialise empty queue (with start = 0)
+    todo: [[int]] = [[0]]
+
+    while len(todo) > 0:
+        print(f"\rQueue: {len(todo)}", end="")
+        # Continue with next chain from queue
+        chain = todo.pop(0)
+        current_joltage = chain[len(chain)-1]
+
+        # Check all possible next adapters and add the new partial chain to the queue
+        for i in range(1, 4):
+            next_joltage = current_joltage + i
+            if next_joltage in adapters:
+                todo.append(chain + [next_joltage])
+
+        # Check if we can reach the target
+        if current_joltage >= target - 3:
+            # Finish this branch
+            result.append(chain)
+
+    return result
+
+
 if __name__ == '__main__':
     input10 = read_input('input/day10')
 
@@ -33,3 +60,7 @@ if __name__ == '__main__':
     print(f"Puzzle 1: {puzzle1[1] * puzzle1[3]} (differences: {puzzle1})")
     print(f"   Time: {time.time() - t1}")
 
+    t1 = time.time()
+    puzzle2 = get_adapter_chain_arrangements(input10)
+    print(f"\nPuzzle 2: {len(puzzle2)}")
+    print(f"   Time: {time.time() - t1}")
