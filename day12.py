b5e4ef6f156c1291ab5467a49e3de16be9f6062c
Davvos11 <vosdavid2@gmail.com>
2020-12-14 16:26:46 +0100

    implemented day 12 part 2

--- day12.py
+++ day12.py
@@ -1,3 +1,4 @@
+import math
 import re
 
 
@@ -15,7 +16,7 @@ class Ship:
     def __init__(self, instructions: [(str, int)], start_dir: int = 90, start_coords: (int, int) = (0, 0)):
         self.instructions = instructions
         self.direction = start_dir
-        self.coords = list(start_coords)  # (x,y)
+        self.coords = list(start_coords)  # (y,x)
 
     def update_position(self, instruction: (str, int)):
         direction, value = instruction
@@ -42,9 +43,50 @@ class Ship:
         return sum(abs(c) for c in self.coords)
 
 
+class Ship2(Ship):
+    def __init__(self, instructions: [(str, int)], start_dir: int = 90, start_coords: (int, int) = (0, 0),
+                 waypoint_start: (int, int) = (1, 10)):
+        super().__init__(instructions, start_dir, start_coords)
+        self.waypoint = list(waypoint_start)  # (y,x)
+        self.count = 0
+
+    def update_position(self, instruction: (str, int)):
+        direction, value = instruction
+        if direction == 'F':
+            self.coords[0] += value * self.waypoint[0]
+            self.coords[1] += value * self.waypoint[1]
+        elif direction == 'N':
+            self.waypoint[0] += value
+        elif direction == 'S':
+            self.waypoint[0] -= value
+        elif direction == 'E':
+            self.waypoint[1] += value
+        elif direction == 'W':
+            self.waypoint[1] -= value
+        elif direction == 'R':
+            self.rotate_waypoint(-value)
+        elif direction == 'L':
+            self.rotate_waypoint(value)
+
+    def rotate_waypoint(self, angle: int):
+        angle = math.radians(angle)
+        s = math.sin(angle)
+        c = math.cos(angle)
+
+        new_x = round(self.waypoint[1] * c - self.waypoint[0] * s)
+        new_y = round(self.waypoint[1] * s + self.waypoint[0] * c)
+
+        # print(f"Waypoint: {self.waypoint} -> {[new_y, new_x]}")
+        self.waypoint = [new_y, new_x]
+
+
 if __name__ == '__main__':
     input12 = read_input('input/day12')
 
     ship = Ship(input12)
     ship.follow_instructions()
     print(f"Puzzle 1: MH distance: {ship.get_manhattan_distance()}")
+
+    ship = Ship2(input12)
+    ship.follow_instructions()
+    print(f"Puzzle 2: MH distance: {ship.get_manhattan_distance()}, coords: {ship.coords}, waypoint: {ship.waypoint}")
