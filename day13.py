21cfccca1f6e53fc5411daa0e3b6cae79935214d
Davvos11 <vosdavid2@gmail.com>
2020-12-14 16:48:07 +0100

    implemented day 13 part 1

--- day13.py
+++ day13.py
@@ -1,3 +1,6 @@
+import math
+
+
 def read_input(filename: str) -> (int, [int]):
     with open(filename) as file:
         timestamp = int(file.readline())
@@ -5,6 +8,20 @@ def read_input(filename: str) -> (int, [int]):
         return timestamp, schedule
 
 
+def closest_bus(timestamp: int, busses: [int]):
+    closest: (int, int) = None  # (id, eta)
+    # Loop through busses
+    for bus in busses:
+        eta = bus - timestamp % bus
+        # If we found a shorter wait time, update the result
+        if closest is None or closest[1] > eta:
+            closest = (bus, eta)
+
+    return closest
+
+
 if __name__ == '__main__':
-    input13 = read_input('input/day13_test')
-    print(input13)
+    input13 = read_input('input/day13')
+
+    puzzle1 = closest_bus(input13[0], input13[1])
+    print(f"Part 1: {math.prod(puzzle1)} (closest bus: {puzzle1[0]} in {puzzle1[1]} minutes)")
